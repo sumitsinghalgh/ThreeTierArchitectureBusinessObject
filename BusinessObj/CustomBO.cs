b0ea62bdc8e690a47e9888f9996df344eecc664f
sumitsinghalgh <sumsgl198630pd@gmail.com>
2021-09-24 23:13:52 +0530

    Third Commitment done

---
+++ BusinessObj/CustomBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObj
+{
+   public class CustomBO
+    {
+        public string CustomMessageNumber { get; set; }
+        public string CustomMessage { get; set; }
+    }
+}
---
+++ BusinessObj/UserBO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObj
+{
+   public class UserBO
+    {
+        public int UserID { get; set; }
+        [Display(Name="User Name")]
+        [Required(ErrorMessage ="User name is required.")]
+        public string UserName { get; set; }
+        [Display(Name = "User Address")]
+        [Required(ErrorMessage = "User Address is required.")]
+        public string UserAddress{ get; set; }
+        [Display(Name = "User Email")]
+        [Required(ErrorMessage = "User Email is required.")]
+        [EmailAddress(ErrorMessage ="Please enter valid email address.")]
+        public string UserEmail { get; set; }
+        [Display(Name = "User Phone")]
+        [Required(ErrorMessage = "User Phone is required.")]
+        public string UserPhone { get; set; }
+
+    }
+}
